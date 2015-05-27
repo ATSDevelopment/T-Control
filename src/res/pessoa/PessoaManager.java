639ac0c67b6ed8b8de3fda5c031a7c641c282ba1
Carlos Souza Rodrigues Filho <car.s.r.f@gmail.com>
2015-05-27 09:09:20 -0300

    Pacote Pessoa att 1

--- src/res/pessoa/PessoaManager.java
+++ src/res/pessoa/PessoaManager.java
@@ -7,31 +7,26 @@ import res.general.EntityManager;
 public class PessoaManager implements EntityManager<Pessoa>{
 	@Override
 	public int salvar(Pessoa entity) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int deletar(Pessoa entity) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public Pessoa getById(int id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ArrayList<Pessoa> listar() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ArrayList<Pessoa> listarWhere(String key, String value) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
