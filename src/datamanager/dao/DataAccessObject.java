24e49e821e93a4d0665ae90919bf663801461b51
Carlos Souza Rodrigues Filho <car.s.r.f@gmail.com>
2015-06-14 17:46:23 -0300

    Gerenciamento de classes

--- src/datamanager/dao/DataAccessObject.java
+++ src/datamanager/dao/DataAccessObject.java
@@ -5,6 +5,4 @@ public interface DataAccessObject<Entity> extends ParcialDataAccessObject<Entity
 	public DataAccessResponse getById(int id);
 	
 	public DataAccessResponse listar();
-	
-	public DataAccessResponse listarWhere(String key, String value);
 }
