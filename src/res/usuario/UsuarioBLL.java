e49151f235cfd390692c29cee3d1e0fd850e79e7
Jonas Rodrigues <jonasrodriguesramires@gmail.com>
2015-06-16 15:46:59 -0300

    Pacotes Grupo e Usuário

---
+++ src/res/usuario/UsuarioBLL.java
@@ -0,0 +1,39 @@
+package res.usuario;
+
+import datamanager.BusinessLayoutLayer;
+import datamanager.dao.DataAccessResponse;
+
+
+public class UsuarioBLL implements BusinessLayoutLayer<Usuario> {
+
+
+	@Override
+	public DataAccessResponse salvar(Usuario entity) {
+		UsuarioDAO usuarioDao = new UsuarioDAO();
+		return usuarioDao.salvar(entity, entity.getId()==0);
+	}
+
+	@Override
+	public DataAccessResponse deletar(Usuario entity) {
+		UsuarioDAO usuarioDao = new UsuarioDAO();
+		return usuarioDao.deletar(entity);
+	}
+
+	@Override
+	public DataAccessResponse listar() {
+		UsuarioDAO usuarioDao = new UsuarioDAO();
+		return usuarioDao.listar();
+	}
+
+	@Override
+	public DataAccessResponse getById(int id) {
+		UsuarioDAO usuarioDao = new UsuarioDAO();
+		return usuarioDao.getById(id);
+	}
+
+
+
+	
+
+	
+}
