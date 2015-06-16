ea9b5c4d7f7fb43581e984a72d367b1b37cc2165
Carlos Souza Rodrigues Filho <car.s.r.f@gmail.com>
2015-06-15 21:08:45 -0300

    Setores

--- src/res/setor/Setor.java
+++ src/res/setor/Setor.java
@@ -1,8 +1,16 @@
 package res.setor;
 
 public class Setor {
-
 	private int id;
+	private String sigla;
+	private String nome;
+	
+	public Setor(int id, String sigla, String nome) {
+		super();
+		this.id = id;
+		this.sigla = sigla;
+		this.nome = nome;
+	}
 
 	public int getId() {
 		return id;
@@ -11,8 +19,25 @@ public class Setor {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 	
 	
-	// Só criei para não dar erro na minha parte, mas essa é a parte da bia!!
+	
+	
 
 }
